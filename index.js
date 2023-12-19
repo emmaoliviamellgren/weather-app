3f5f07303328835944e9d5abdd9ca125f312c0e0
Emma Mellgren <mellgrenemma0@gmail.com>
2023-12-19 09:31:35 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const inputValue = document.getElementById('#search-bar')
+const searchBox = document.querySelector('.search-box')
+const searchButton = document.querySelector('button')
+
+console.log(inputValue)
+
+// function logging() {
+//     inputValue.value
+// }
+
+// searchButton.addEventListener('click', => {
+//     searchBox.innerHTML = document.createElement('p')
+//     inputValue.value;
+// });
