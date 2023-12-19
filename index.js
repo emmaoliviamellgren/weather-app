4ddaa4a7b87165f616b18c6a512ee1b33868c2d0
Emma Mellgren <mellgrenemma0@gmail.com>
2023-12-19 16:41:03 +0100

    Added get request + API

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+const weather = {
+    API_KEY: '550f2bc9f0aa46b5945111518231412',
+    
+} 
+
 const inputValue = document.getElementById('#search-bar')
 const searchBox = document.querySelector('.search-box')
 const searchButton = document.querySelector('button')
 
 console.log(inputValue)
 
-// function logging() {
-//     inputValue.value
-// }
-
-// searchButton.addEventListener('click', => {
-//     searchBox.innerHTML = document.createElement('p')
-//     inputValue.value;
-// });
+fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY.value}&q=Stockholm`)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    
+});
