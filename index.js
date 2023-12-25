b20b91e4e71fd9919db35277d0b15155d17c640d
Emma Mellgren <mellgrenemma0@gmail.com>
2023-12-25 18:30:41 +0100

    Added API-key

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const weather = {
-    API_KEY: '550f2bc9f0aa46b5945111518231412',
-    
-} 
+    API_KEY: '550f2bc9f0aa46b5945111518231412'
+    }
 
-const inputValue = document.getElementById('#search-bar')
+const inputValue = document.getElementById('search-bar')
 const searchBox = document.querySelector('.search-box')
 const searchButton = document.querySelector('button')
 
-console.log(inputValue)
-
-fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY.value}&q=Stockholm`)
+fetch(`http://api.weatherapi.com/v1/current.json?key=550f2bc9f0aa46b5945111518231412&q=Stockholm`)
 .then(response => response.json())
 .then(data => {
     console.log(data)
